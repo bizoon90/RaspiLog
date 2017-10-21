e0caf455848ecfa841f3ab8efeedd1a6180c4c09
bizoon90 <32790873+bizoon90@users.noreply.github.com>
2017-10-21 10:44:06 +0200

    Update raspilog.py

--- raspilog.py
+++ raspilog.py
@@ -1,4 +1,5 @@
 import os 
+from time import sleep, strftime, time
 #CPUtemperature                                  
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
@@ -8,14 +9,14 @@ def getCPUuse():
     return(str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip(\
 )))
 def main():
-     # CPU
-     CPU_temp = getCPUtemperature()
-     CPU_usage = getCPUuse()
-     #Logging
-     f = open("/log.txt","w")
-     f.write("CPU Temperature :"""+CPU_temp+"\n\r")
-     f.write("\n\r" + "CPU Usage:" +CPU_usage+"%"+"\n\r")
-     f.close()	
+     with open("cpu_temp.csv", "a") as log:
+          while True:
+              CPU_temp = getCPUtemperature()
+              CPU_usage = getCPUuse()
+              log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(CPU_temp),"\n\n"))
+			  log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(CPU_usage),"\n\n"))
+              sleep(1)
+    
 if __name__ == '__main__':
-    main()
+main()
 
